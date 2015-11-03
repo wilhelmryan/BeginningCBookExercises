97ece316f19d1087ba2b7ca23655ca646dc87ff5
wilhelmryan <jhiscockwagner@yahoo.com>
2015-11-02 20:19:45 -0800

    Create 01.c

---
+++ Chapter-25/01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char loop;
+
+    puts("Presenting the alphabet: ");
+    for(loop='A';loop<='Z';loop++)
+        putchar(loop);
+
+    return(0);
+}

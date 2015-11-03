d0530fef577d8a4a5f947b98f437ac6bc50a6553
wilhelmryan <jhiscockwagner@yahoo.com>
2015-11-02 19:45:17 -0800

    Create 10.c

---
+++ Chapter-19/10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+   char alpha='A';
+   int x;
+   char *pa;
+
+   pa=&alpha;
+
+   for(x=0;x<26;x++)
+   {
+       putchar((*pa)++);
+   }
+   putchar('\n');
+
+   return(0);
+}

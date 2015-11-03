25fb35d13df648ecfb6b6cd3a41c1a8e514c2e00
wilhelmryan <jhiscockwagner@yahoo.com>
2015-11-02 20:09:45 -0800

    Create 04.c

---
+++ Chapter-23/04.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <time.h>
+#include <sys/stat.h>
+
+int main()
+{
+    DIR *folder;
+    struct dirent *file;
+    struct stat filestat;
+
+    folder=opendir(".");
+    if(folder==NULL)
+    {
+        puts("There was a problem with the folder");
+        exit(1);
+    }
+
+    while(file=readdir(folder))
+    {
+        stat(file->d_name,&filestat);
+
+        if(S_ISDIR(filestat.st_mode))
+        {
+            printf("%-14s <DIR> %s",file->d_name,ctime(&filestat.st_mtime));
+        }
+        else
+        {
+            printf("%-14s %51d %s",file->d_name,(long)filestat.st_size,ctime(&filestat.st_mtime));
+        }
+    }
+    closedir(folder);
+
+    return(0);
+}

f933762fd4182e7592bf98ab8fad308b3e07a661
wilhelmryan <jhiscockwagner@yahoo.com>
2015-11-02 19:46:45 -0800

    Create 14.c

---
+++ Chapter-19/14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   char sample[]="From whence cometh my help?\n";
+   char *index;
+
+   index=sample;
+
+   while(*index)
+   {
+       putchar(*index++);
+   }
+   return(0);
+}

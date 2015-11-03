bd86d096167265a1ecaaada43d8b485d8839643d
wilhelmryan <jhiscockwagner@yahoo.com>
2015-11-02 19:37:41 -0800

    Create 05.c

---
+++ Chapter-18/05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    struct robot {
+    int alive;
+    char name[5];
+    int xpos;
+    int ypos;
+    int strength;
+    };
+
+    printf("The evil robot struct size is %u\n", sizeof(struct robot));
+
+    return(0);
+
+}
